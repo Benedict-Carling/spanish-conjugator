712589b0b510f3dc5800e30ce2032e8038002704
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-14 15:42:44 +0100

    adds pytest

---
+++ tests/test_spanishconjugator.py
@@ -0,0 +1,5 @@
+from spanishconjugator import Conjugator
+
+def test_imperfect_imperitive_yo_ar():
+    expected = "hablaba"
+    assert Conjugator().conjugate('hablar','imperfect','imperitive','yo') == expected
