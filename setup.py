17132581f2337aa4e2dfef14983791e9f78eea4b
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-14 17:35:39 +0100

    renames test file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spanishconjugator',
-    version='0.0.5',
+    version='0.0.6',
     description='A python library to conjugate spanish words with parameters tense, mood and pronoun',
     py_modules=["spanishconjugator"],
     package_dir={'': 'src'},
