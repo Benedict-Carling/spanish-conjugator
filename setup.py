b1b6fc5fd8830cb5eaf367d4bca7c99cd29ddbdf
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-14 15:22:10 +0100

    adds classifiers to setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,17 @@ from setuptools import setup
 
 setup(
     name='spanishconjugator',
-    version='0.0.2',
+    version='0.0.3',
     description='A python library to conjugate spanish words with parameters tense, mood and pronoun',
     py_modules=["spanishconjugator"],
     package_dir={'': 'src'},
+    classifiers = [
+        "Programming Language :: python :: 3",
+        "Programming Language :: python :: 3.6",
+        "Programming Language :: python :: 3.7",
+        "Programming Language :: python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent"
+    ]
 )
