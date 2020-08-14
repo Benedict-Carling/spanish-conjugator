21c010895e8eece617c13850bae0b79f305bf86c
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-14 16:23:24 +0100

    adds twine to dev requires

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spanishconjugator',
-    version='0.0.3',
+    version='0.0.5',
     description='A python library to conjugate spanish words with parameters tense, mood and pronoun',
     py_modules=["spanishconjugator"],
     package_dir={'': 'src'},
     classifiers = [
-        "Programming Language :: python :: 3",
-        "Programming Language :: python :: 3.6",
-        "Programming Language :: python :: 3.7",
-        "Programming Language :: python :: 3.8",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent"
@@ -23,7 +23,8 @@ setup(
     extras_require={
         "dev": [
             "pytest>=6.0.1",
-            "check-manifest>=0.42"
+            "check-manifest>=0.42",
+            "twine>=3.2.0"
         ],
     },
     url="https://github.com/Benedict-Carling/spanish-conjugator",
