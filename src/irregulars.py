9ed69d86aa9397ce917dd27b623b846cb4696499
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-15 14:44:19 +0100

    adds irregular handling

---
+++ src/irregulars.py
@@ -0,0 +1,40 @@
+# -*- coding: iso-8859-15 -*-
+
+irregulars = {
+    'ir':{
+        'imperitive':{
+            'imperfect':{
+                'yo':'iba',
+                'tu':'ibas',
+                'usted':'iba',
+                'nosotros':'íbamos',
+                'vosotros':'ibais',
+                'ustedes':'iban'
+            }
+        }
+    },
+    'ser':{
+        'imperitive':{
+            'imperfect':{
+                'yo':'era',
+                'tu':'eras',
+                'usted':'era',
+                'nosotros':'éramos',
+                'vosotros':'erais',
+                'ustedes':'eran'    
+            }
+        }
+    },
+    'ver':{
+        'imperitive':{
+            'imperfect':{
+                'yo':'veía',
+                'tu':'veías',
+                'usted':'veía',
+                'nosotros':'veíamos',
+                'vosotros':'veíais',
+                'ustedes':'veían'
+            }
+        }
+    },
+}
--- src/spanishconjugator.py
+++ src/spanishconjugator.py
@@ -1,9 +1,7 @@
 # -*- coding: iso-8859-15 -*-
+from irregulars import irregulars
 class Conjugator():
 
-    # Present Conjugations
-
-    # Past Conjugations
 
     def conjugate(self,root_verb,tense,mood,pronoun):
         tense = tense.lower()
@@ -14,17 +12,22 @@ class Conjugator():
             if mood == "imperitive":
                 if pronoun == "yo":
                     if root_verb[-2:] == "ar":
-                        conjugation = root_verb[:-2] + "aba"
+                        try:
+                            conjugation = irregulars[root_verb][index][pronoun]
+                        except:
+                            conjugation = root_verb[:-2] + "aba"
                         return conjugation
                     if root_verb[-2:] == "er" or "ir":
-                        conjugation = root_verb[:-2] + "ía"
+                        try:
+                            conjugation = irregulars[root_verb][mood][tense][pronoun]
+                        except:
+                            conjugation = root_verb[:-2] + "ía"
                         return conjugation
 
         return conjugation
-
-    # Future Conjugations
 
 # Example test to run -- later to be turned into unit tests
 #conjugator = Spanish_conjugator()
 print(Conjugator().conjugate('hablar','imperfect','imperitive','yo'))
 print(Conjugator().conjugate('charlar','imperfect','imperitive','yo'))
+print(Conjugator().conjugate('ver','imperfect','imperitive','yo'))
