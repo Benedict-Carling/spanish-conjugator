3388341828c70d3f482885e07c1298955474311b
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-23 17:19:09 +0100

    passing pytest

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spanishconjugator',
-    version='1.0.0',
+    version='1.0.1',
     description='A python library to conjugate spanish words with parameters tense, mood and pronoun',
     py_modules=["spanishconjugator"],
     package_dir={'': 'src'},
