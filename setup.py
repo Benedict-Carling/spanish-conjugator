04020f01c92f0dfbad13818e6b95ee95724ae428
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-25 17:56:51 +0100

    bumps to version 2.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='spanishconjugator',
-    version='2.0.1',
+    version='2.0.2',
     description='A python library to conjugate spanish words with parameters tense, mood and pronoun',
     py_modules=["spanishconjugator"],
     package_dir={'': 'src'},
