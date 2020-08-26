9bc01913aacf96926593d690b7d3e45b1912e8a5
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-08-26 16:10:38 +0100

    adds package system keep file size small

--- src/spanishconjugator.py
+++ src/spanishconjugator.py
@@ -1,4 +1,5 @@
 # -*- coding: iso-8859-15 -*-
+from tenses.indicitive.preterite import indicitive_preterite
 # ------------------- Irregulars ------------------
 irregulars = {
     'ir':{
@@ -285,84 +286,7 @@ class Conjugator():
 # ----------------------------------- Preterite Indicitive ----------------------------------- #
         if tense == "preterite":
             if mood == "indicitive":
-                if pronoun == "yo":
-                    if root_verb[-2:] == "ar":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "é"
-                        return conjugation
-                    if root_verb[-2:] == "er" or "ir":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "í"
-                        return conjugation
-                if pronoun == "tu":
-                    if root_verb[-2:] == "ar":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "aste"
-                        return conjugation
-                    if root_verb[-2:] == "er" or "ir":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "iste"
-                        return conjugation
-                if pronoun == "usted":
-                    if root_verb[-2:] == "ar":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "ó"
-                        return conjugation
-                    if root_verb[-2:] == "er" or "ir":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "ió"
-                        return conjugation
-                if pronoun == "nosotros":
-                    if root_verb[-2:] == "ar":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "amos"
-                        return conjugation
-                    if root_verb[-2:] == "er" or "ir":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "imos"
-                        return conjugation
-                if pronoun == "vosotros":
-                    if root_verb[-2:] == "ar":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "asteis"
-                        return conjugation
-                    if root_verb[-2:] == "er" or "ir":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "isteis"
-                        return conjugation
-                if pronoun == "ustedes":
-                    if root_verb[-2:] == "ar":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "aron"
-                        return conjugation
-                    if root_verb[-2:] == "er" or "ir":
-                        try:
-                            conjugation = irregulars[root_verb][mood][tense][pronoun]
-                        except:
-                            conjugation = root_verb[:-2] + "ieron"
-                        return conjugation
+                conjugation = indicitive_preterite(root_verb, pronoun)
 
 # ----------------------------------- Future Simple Indicitive ----------------------------------- #
         if tense == "future":
@@ -410,4 +334,3 @@ class Conjugator():
                             conjugation = root_verb + "án"
                         return conjugation
         return conjugation
-        
