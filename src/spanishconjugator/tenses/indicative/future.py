2f1c894b834c422e0d46b2fab740f7dc7133b50f
shrutiichandra <shruti.chandra@students.iiit.ac.in>
2021-10-16 22:22:20 +0530

    modified indicative tenses

--- src/spanishconjugator/tenses/indicative/future.py
+++ src/spanishconjugator/tenses/indicative/future.py
@@ -1,31 +1,86 @@
 # -*- coding: iso-8859-15 -*-
-def indicative_future(root_verb, pronoun):
-    if pronoun == "yo":
+from spanishconjugator.irregulars.irregular_dict              import irregulars_dictionary
+def indicative_future_first_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["future"]["yo"]
+        return conjugation
+    except:
         if root_verb[-2:] == "ar" or "er" or "ir":
             conjugation = root_verb + "é"
             return conjugation
 
-    if pronoun == "tu":
-        if root_verb[-2:] == "ar" or "er" or "ir":
-            conjugation = root_verb + "ás"
-            return conjugation
-            
-    if pronoun == "usted":
-        if root_verb[-2:] == "ar" or "er" or "ir":
-            conjugation = root_verb + "á"
-            return conjugation
-
-    if pronoun == "nosotros":
+def indicative_future_first_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["future"]["nosotros"]
+        return conjugation
+    except:
         if root_verb[-2:] == "ar" or "er" or "ir":
             conjugation = root_verb + "emos"
             return conjugation
 
-    if pronoun == "vosotros":
+def indicative_future_second_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["future"]["tu"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar" or "er" or "ir":
+            conjugation = root_verb + "ás"
+            return conjugation
+
+def indicative_future_second_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["future"]["vosotros"]
+        return conjugation
+    except:
         if root_verb[-2:] == "ar" or "er" or "ir":
             conjugation = root_verb + "éis"
             return conjugation
 
-    if pronoun == "ustedes":
+def indicative_future_third_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["future"]["usted"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar" or "er" or "ir":
+            conjugation = root_verb + "á"
+            return conjugation
+
+def indicative_future_third_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["future"]["ustedes"]
+        return conjugation
+    except:
         if root_verb[-2:] == "ar" or "er" or "ir":
             conjugation = root_verb + "án"
             return conjugation
+
+def indicative_future(root_verb, pronoun):
+    if pronoun == "yo":
+        return indicative_future_first_person_singular(root_verb)
+
+    elif pronoun == "tu":
+        return indicative_future_second_person_singular(root_verb)
+        
+    elif pronoun == "usted" or pronoun == "el" or pronoun == "ella":
+        return indicative_future_third_person_singular(root_verb)
+        
+    elif pronoun == "nosotros":
+        return indicative_future_first_person_plural(root_verb)
+        
+    elif pronoun == "vosotros":
+        return indicative_future_second_person_plural(root_verb)
+        
+    elif pronoun == "ustedes" or pronoun == "ellos" or pronoun == "ellas":
+        return indicative_future_third_person_plural(root_verb)
+
+    else:
+        # no pronoun is given
+        _dict = {'el/ella/usted': indicative_future_third_person_singular(root_verb), \
+                 'ellos/ellas/ustedes':indicative_future_third_person_plural(root_verb),\
+                 'tu':indicative_future_second_person_singular(root_verb),\
+                 'vosotros':indicative_future_second_person_plural(root_verb),\
+                 'yo':indicative_future_first_person_singular(root_verb),\
+                 'nosotros':indicative_future_first_person_plural(root_verb)\
+                }
+        return _dict
+        
--- src/spanishconjugator/tenses/indicative/preterite.py
+++ src/spanishconjugator/tenses/indicative/preterite.py
@@ -1,38 +1,46 @@
 # -*- coding: iso-8859-15 -*-
-def indicative_preterite(root_verb, pronoun):
-    if pronoun == "yo":
+from spanishconjugator.irregulars.irregular_dict              import irregulars_dictionary
+def indicative_preterite_first_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["preterite"]["yo"]
+        return conjugation
+    except:
         if root_verb[-2:] == "ar":
             conjugation = root_verb[:-2] + "é"
             return conjugation
         if root_verb[-2:] == "er" or "ir":
             conjugation = root_verb[:-2] + "í"
             return conjugation
-    if pronoun == "tu":
-        
-        if root_verb[-2:] == "ar":
-            conjugation = root_verb[:-2] + "aste"
-            return conjugation
-        if root_verb[-2:] == "er" or "ir":
-            conjugation = root_verb[:-2] + "iste"
-            return conjugation
-    if pronoun == "usted":
 
-        if root_verb[-2:] == "ar":
-            conjugation = root_verb[:-2] + "ó"
-            return conjugation
-        if root_verb[-2:] == "er" or "ir":
-            conjugation = root_verb[:-2] + "ió"
-            return conjugation
-    if pronoun == "nosotros":
-
+def indicative_preterite_first_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["preterite"]["nosotros"]
+        return conjugation
+    except:
         if root_verb[-2:] == "ar":
             conjugation = root_verb[:-2] + "amos"
             return conjugation
         if root_verb[-2:] == "er" or "ir":
             conjugation = root_verb[:-2] + "imos"
             return conjugation
-    if pronoun == "vosotros":
 
+def indicative_preterite_second_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["preterite"]["tu"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar":
+            conjugation = root_verb[:-2] + "aste"
+            return conjugation
+        if root_verb[-2:] == "er" or "ir":
+            conjugation = root_verb[:-2] + "iste"
+            return conjugation
+
+def indicative_preterite_second_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["preterite"]["vosotros"]
+        return conjugation
+    except:
         if root_verb[-2:] == "ar":
             conjugation = root_verb[:-2] + "asteis"
             return conjugation
@@ -40,10 +48,56 @@ def indicative_preterite(root_verb, pronoun):
             conjugation = root_verb[:-2] + "isteis"
             return conjugation
 
-    if pronoun == "ustedes":
+def indicative_preterite_third_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["preterite"]["usted"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar":
+            conjugation = root_verb[:-2] + "ó"
+            return conjugation
+        if root_verb[-2:] == "er" or "ir":
+            conjugation = root_verb[:-2] + "ió"
+            return conjugation
+
+def indicative_preterite_third_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["indicative"]["preterite"]["ustedes"]
+        return conjugation
+    except:
         if root_verb[-2:] == "ar":
             conjugation = root_verb[:-2] + "aron"
             return conjugation
         if root_verb[-2:] == "er" or "ir":
             conjugation = root_verb[:-2] + "ieron"
             return conjugation
+
+def indicative_preterite(root_verb, pronoun):
+    if pronoun == "yo":
+        return indicative_preterite_first_person_singular(root_verb)
+
+    elif pronoun == "tu":
+        return indicative_preterite_second_person_singular(root_verb)
+        
+    elif pronoun == "usted" or pronoun == "el" or pronoun == "ella":
+        return indicative_preterite_third_person_singular(root_verb)
+        
+    elif pronoun == "nosotros":
+        return indicative_preterite_first_person_plural(root_verb)
+        
+    elif pronoun == "vosotros":
+        return indicative_preterite_second_person_plural(root_verb)
+        
+    elif pronoun == "ustedes" or pronoun == "ellos" or pronoun == "ellas":
+        return indicative_preterite_third_person_plural(root_verb)
+
+    else:
+        # no pronoun is given
+        _dict = {'el/ella/usted': indicative_preterite_third_person_singular(root_verb), \
+                 'ellos/ellas/ustedes':indicative_preterite_third_person_plural(root_verb),\
+                 'tu':indicative_preterite_second_person_singular(root_verb),\
+                 'vosotros':indicative_preterite_second_person_plural(root_verb),\
+                 'yo':indicative_preterite_first_person_singular(root_verb),\
+                 'nosotros':indicative_preterite_first_person_plural(root_verb)\
+                }
+        return _dict
