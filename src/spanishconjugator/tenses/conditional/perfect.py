f8d541c5f00a1e49442c2a3284a76a177604a1b1
shrutiichandra <shruti.chandra@students.iiit.ac.in>
2022-04-03 18:39:29 +0530

    modified perfect conditional

--- src/spanishconjugator/tenses/conditional/perfect.py
+++ src/spanishconjugator/tenses/conditional/perfect.py
@@ -1,49 +1,108 @@
 # -*- coding: iso-8859-15 -*-
+from spanishconjugator.irregulars.irregular_dict              import irregulars_dictionary
+def conditional_perfect_first_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["conditional"]["perfect_conditional"]["yo"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar":
+            conjugation = root_verb[:-2] + "ado"
+            return "habría " + conjugation
+        if root_verb[-2:] == "er" or "ir":
+            conjugation = root_verb[:-2] + "ido"
+            return "habría " + conjugation
+
+def conditional_perfect_first_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["conditional"]["perfect_conditional"]["nosotros"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar":
+            conjugation = root_verb[:-2] + "ado"
+            return "habríamos " + conjugation
+        if root_verb[-2:] == "er" or "ir":
+            conjugation = root_verb[:-2] + "ido"
+            return "habríamos " + conjugation
+        
+
+def conditional_perfect_second_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["conditional"]["perfect_conditional"]["tu"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar":
+            conjugation = root_verb[:-2] + "ado"
+            return "habrías " + conjugation
+        if root_verb[-2:] == "er" or "ir":
+            conjugation = root_verb[:-2] + "ido"
+            return "habrías " + conjugation
+
+def conditional_perfect_second_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["conditional"]["perfect_conditional"]["vosotros"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar":
+            conjugation = root_verb[:-2] + "ado"
+            return "habríais " + conjugation
+        if root_verb[-2:] == "er" or "ir":
+            conjugation = root_verb[:-2] + "ido"
+            return "habríais " + conjugation
+
+def conditional_perfect_third_person_singular(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["conditional"]["perfect_conditional"]["usted"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar":
+            conjugation = root_verb[:-2] + "ado"
+            return "habría " + conjugation
+        if root_verb[-2:] == "er" or "ir":
+            conjugation = root_verb[:-2] + "ido"
+            return "habría " + conjugation
+
+def conditional_perfect_third_person_plural(root_verb):
+    try:
+        conjugation = irregulars_dictionary[root_verb]["conditional"]["perfect_conditional"]["ustedes"]
+        return conjugation
+    except:
+        if root_verb[-2:] == "ar":
+            conjugation = root_verb[:-2] + "ado"
+            return "habrían " + conjugation
+        if root_verb[-2:] == "er" or "ir":
+            conjugation = root_verb[:-2] + "ido"
+            return "habrían " + conjugation
+
 def conditional_perfect(root_verb, pronoun):
     if pronoun == "yo":
-        if root_verb[-2:] == "ar":
-            conjugation = root_verb[:-2] + "ado"
-            return "habría " + conjugation
-        if root_verb[-2:] == "er" or "ir":
-            conjugation = root_verb[:-2] + "ido"
-            return "habría " + conjugation
+        return conditional_perfect_first_person_singular(root_verb)
+            
+    elif pronoun == "tu":
+        return conditional_perfect_second_person_singular(root_verb)
 
-    if pronoun == "tu":
-        if root_verb[-2:] == "ar":
-            conjugation = root_verb[:-2] + "ado"
-            return "habrías " + conjugation
-        if root_verb[-2:] == "er" or "ir":
-            conjugation = root_verb[:-2] + "ido"
-            return "habrías " + conjugation
 
-    if pronoun == "usted":
-        if root_verb[-2:] == "ar":
-            conjugation = root_verb[:-2] + "ado"
-            return "habría " + conjugation
-        if root_verb[-2:] == "er" or "ir":
-            conjugation = root_verb[:-2] + "ido"
-            return "habría " + conjugation
+    elif pronoun == "usted" or pronoun == "el" or pronoun == "ella":
+        return conditional_perfect_third_person_singular(root_verb)
 
-    if pronoun == "nosotros":
-        if root_verb[-2:] == "ar":
-            conjugation = root_verb[:-2] + "ado"
-            return "habríamos " + conjugation
-        if root_verb[-2:] == "er" or "ir":
-            conjugation = root_verb[:-2] + "ido"
-            return "habríamos " + conjugation
 
-    if pronoun == "vosotros":
-        if root_verb[-2:] == "ar":
-            conjugation = root_verb[:-2] + "ado"
-            return "habríais " + conjugation
-        if root_verb[-2:] == "er" or "ir":
-            conjugation = root_verb[:-2] + "ido"
-            return "habríais " + conjugation
+    elif pronoun == "nosotros":
+        return conditional_perfect_first_person_plural(root_verb)
 
-    if pronoun == "ustedes":
-        if root_verb[-2:] == "ar":
-            conjugation = root_verb[:-2] + "ado"
-            return "habrían " + conjugation
-        if root_verb[-2:] == "er" or "ir":
-            conjugation = root_verb[:-2] + "ido"
-            return "habrían " + conjugation
+
+    elif pronoun == "vosotros":
+        return conditional_perfect_second_person_plural(root_verb)
+
+
+    elif pronoun == "ustedes" or pronoun == "ellos" or pronoun == "ellas":
+        return conditional_perfect_third_person_plural(root_verb)
+
+    else:
+        # no pronoun is given
+        _dict = {'el/ella/usted': conditional_perfect_third_person_singular(root_verb), \
+                 'ellos/ellas/ustedes':conditional_perfect_third_person_plural(root_verb),\
+                 'tu':conditional_perfect_second_person_singular(root_verb),\
+                 'vosotros':conditional_perfect_second_person_plural(root_verb),\
+                 'yo':conditional_perfect_first_person_singular(root_verb),\
+                 'nosotros':conditional_perfect_first_person_plural(root_verb)\
+                }
+        return _dict
