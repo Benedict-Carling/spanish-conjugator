4786d821958c0dc393464f0e2d1e3dac28485c2d
Benedict-Carling <Benedict-Carling@users.noreply.github.com>
2022-07-16 20:27:15 +0000

    [skip ci]Automated version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="spanishconjugator",
-    version="2.3.2471",
+    version="2.3.2472",
     description="A python library to conjugate spanish words with parameters tense, mood and pronoun",
     packages=[
         "spanishconjugator",
